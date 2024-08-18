28f44fa6805ca9965eb35c35261081f86287400b
VoloshchukMark <voloshchuk.mark@chnu.edu.ua>
2024-08-18 15:29:39 +0300

    Add some base functions to Teacher class

--- Journal/Headers/Teacher.h
+++ Journal/Headers/Teacher.h
@@ -1,16 +1,38 @@
 #ifndef TEACHER_H
 #define TEACHER_H
 
+#include "Person.h"
 
-class Teacher
+#include <iostream>
+#include <vector>
+
+class Teacher :public Person
 {
-    public:
-        Teacher();
-        virtual ~Teacher();
+private:
+//    vector<Subject> subjects;
+    std::string address;
+public:
 
-    protected:
+    std::string getAddress();
 
-    private:
+    void setAddress(std::string newAddress);
+
+    void displayId();
+    void displayName();
+    void displaySurname();
+    void displayPatronymic();
+    void displayAge();
+    void displaySex();
+    void displayAddress();
+
+    Teacher();
+    Teacher(int newId);
+    Teacher(int newId, std::string newName);
+    Teacher(int newId, std::string newName, std::string newSurname);
+    Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic);
+    Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic, int newAge);
+    Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic, int newAge, std::string newSex);
+    Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic, int newAge, std::string newSex, std::string newAddress);
 };
 
 #endif // TEACHER_H
--- Journal/Sources/Teacher.cpp
+++ Journal/Sources/Teacher.cpp
@@ -1,11 +1,50 @@
-#include "Teacher.h"
+#include "../Headers/Teacher.h"
+
+#include "../Headers/Person.h"
+
+#include <iostream>
+
+
+std::string Teacher::getAddress()
+{
+    return address;
+}
+
+void Teacher::setAddress(std::string newAddress)
+{
+    address = newAddress;
+}
+
+void Teacher::displayId(){
+    std::cout << "ID: " << getId() << std::endl;
+}
+void Teacher::displayName(){
+    std::cout << "Name: " << getName() << std::endl;
+}
+void Teacher::displaySurname(){
+    std::cout << "Surname: " << getSurname() << std::endl;
+}
+void Teacher::displayPatronymic(){
+    std::cout << "Patronymic: " << getPatronymic() << std::endl;
+}
+void Teacher::displayAge(){
+    std::cout << "Age: " << getAge() << std::endl;
+}
+void Teacher::displaySex(){
+    std::cout << "Sex: " << getSex() << std::endl;
+}
 
 Teacher::Teacher()
-{
-    //ctor
-}
-
-Teacher::~Teacher()
-{
-    //dtor
-}
+    :Teacher(0, "Unknown", "Unknown", "Unknown", 0, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId)
+    :Teacher(newId, "Unknown", "Unknown", "Unknown", 0, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId, std::string newName)
+    :Teacher(newId, newName, "Unknown", "Unknown", 0, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId, std::string newName, std::string newSurname)
+    :Teacher(newId, newName, newSurname, "Unknown", 0, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic)
+    :Teacher(newId, newName, newSurname, newPatronymic, 0, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic, int newAge)
+    :Teacher(newId, newName, newSurname, newPatronymic, newAge, "Unknown", "Unknown") {}
+Teacher::Teacher(int newId, std::string newName, std::string newSurname, std::string newPatronymic, int newAge, std::string newSex, std::string newAddress)
+    :Person(newId, newName, newSurname, newPatronymic, newAge, newSex), address{newAddress} {}
